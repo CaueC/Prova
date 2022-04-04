529dd720ae677bfe2ab069de5e065009bc1f251a
CaueC <caue.coube@fatec.sp.gov.br>
2022-04-04 17:03:13 +0000

    Salvando

---
+++ BalancoTrimestral.java
@@ -0,0 +1,11 @@
+public class BalancoTrimestral{
+    public static void main(String[] args) {
+
+        double gastosAbril = 1212.00; // salário mínimo.
+
+        System.out.printf("Valor do salário mínimo: %.2f R$",gastosAbril," R$");
+        System.out.printf("\nValor do salário mínimo multiplicado x10: %.2f R$", (gastosAbril * 10)," R$");
+        
+    }
+
+}
